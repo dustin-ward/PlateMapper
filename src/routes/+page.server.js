fdbb5cff297bb38261f54e939343b3b1fbdd0b32
dustin-ward <wardddustin@gmail.com>
2024-02-10 14:22:11 -0500

    pre un-ts

---
+++ src/routes/+page.server.js
@@ -0,0 +1,8 @@
+export function load() {
+	return {
+		completed: [
+			{id: "CA-AB", status: true},
+			{id: "CA-ON", status: true}
+		]
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	export let data;
+
 	import '../app.css';
 	import type { LatLngExpression } from 'leaflet';
 	import Leaflet from '$lib/Leaflet.svelte';
@@ -6,31 +8,9 @@
 	import Popup from '$lib/Popup.svelte';
 
 	const initialView: LatLngExpression = [49.328325, -100.283634];
-	const markerLocations: Array<LatLngExpression> = [
-		[51.513870009926, 7.473969975241]
-	];
 </script>
 
 <div class="w-full h-screen">
-	<Leaflet view={initialView} zoom={5}>
-		{#each markerLocations as latLng}
-			<Marker {latLng} width={40} height={40}>
-				<!-- ShipBit Icon -->
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					xml:space="preserve"
-					style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"
-					viewBox="0 0 45 40"
-				>
-				<path
-					d="m23.046 25.449 9.601 16.885H13.253l9.793-16.885ZM45 23.965H25.702l9.575 16.84L45 23.965ZM44.963 20.923 35.339 4.254l-9.668 16.669h19.292ZM32.771 2.618h-4.17L8.522 37.237l2.08 3.603L32.771 2.618ZM25.084 2.618H11.465L0 22.476l6.768 11.722 18.316-31.58Z"
-					style="fill:#e9204f;fill-rule:nonzero"
-					transform="translate(0 -2.618)"
-				/>
-				</svg>
-
-				<Popup>Test Popup</Popup>
-			</Marker>
-		{/each}
+	<Leaflet view={initialView} completed={data.completed} zoom={5}>
 	</Leaflet>
 </div>
